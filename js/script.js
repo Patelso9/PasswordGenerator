17b05b598a52a0d231436f815f31845b96e78f3c
Princess <sonali.patel.pkg@gmail.com>
2021-05-08 02:31:39 -0400

    added code and broke more code. good night

--- js/script.js
+++ js/script.js
@@ -1,109 +1,99 @@
-// ref all characters
-var specialChars = ["!", "@", "#", "$", "%", "&"];
-var numbArray= '0123456789'.split(' ');
-var lowerCase= 'abcdefghijklmnopqrstuvwxyz'.split(' ');
-var upperCase= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(' ');
-
-// funct to prompt user for password options
-function getPassword(){
-  // create var to store length of password
-  var length = parseInt (
-    prompt("How many characters would you like your password to be? \nMust be atlest 8 characters long")
-  );
-
-    // create conditional statement to check if password length is a num
-  if(Number.isNaN(length)) {
-    alert("password must be a number")
-    return null;
-  };
-
-    // cond state to check if pswd is atleast 8 char
-  if (length < 8) {
-    alert("password length must be atleast 8 characters")
-    return null;
-  };
-  
-    // conditional statement to check if pswd length is atleas 128 characters
-  if (length > 128) {
-    alert("password can not contain more than 128 characters")
-    return null;
-  };
-  // create confirmation prompts for types of characters the user wants to use
-  specialChars = window.confirm("Do you want special characters?");
-  numbArray = window.confirm("Do you want numbers?");
-  lowerCase = window.confirm("Do you want lowercase leters?");
-  upperCase = window.confirm("Do you want upper case letters?");
-  // create conditional statement to ensure user clicks one of the character types
-  
-
-
-  // create an object to store user input
-    var possiblePass = {
-      // store lenth & all characters selected by user
-      length: length.length, 
-      specialChars: specialChars.length,
-      numbArray: numbArray.length,
-      lowerCase: lowerCase.length,
-      upperCase: upperCase.length,
-    }
-
-    return possiblePass;
-
-}
-
-// create function for getting random element from array
-  function getRandomLetters(arr) {
-    var random = possiblePass.arr(Math.floor(math.random() * possiblePass.length));    //some kind of math problem arr.length
-    // save to a variable (r,p,s)
-    var rand = arr[random]   // future research: get 1 of each character, no repeats
-
-    return rand;
-  }
-
-// create fuction to generate passwork w. user input
-function generatePassword() {
-  // set a variable to call my getPassword function
-  var options = getPassword();
-
-  // variable to store the password as it is being concatenated 
-  var passResult = [];
-
-  // create variable to store types of characters to include in password
-  var possibleChars = [];
-
-  // create variable to store ea.trype of garentied characters
-  var guarChars = [];
-
-  // condition that adds array for special characters into the arrya of possible characters based on user input
-  
-  
-  // push new random characters into gaurenteed characters
-  if (options.specialChars) {
-    possibleChars = possibleChars.concat(specialChars);
-    guarChars.push(getRandomLetters(specialChars))
-  }
-  // create for loop to iterate over password length from the options object
-  for (var i=0; i < options.length; i++) {
-    var possibleCharacter = getRandomLetters(possibleCharacter);
-  
-    passResult.push(possibleCharacter)
-  }
-
-  return passResult.join('');
-}
-
+// Variables for all characters
+var passArray = [];
+var specialChars = ["!","@","#","$","%","^","&","*","(",")","{","}","|","[","]",";","'",":","<",">","?","/"];
+var numbArray= ["1","2","3","4","5","6","7","8","9","0"];
+var lowerCase= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","X","Y","Z"];
 
 //  Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function generatePassword() {
+function writePassword() {
+  
+  var passwordString = "";
+  
+  // create var to store length of password
+  var passwordLength = parseInt (
+    prompt("How many characters would you like your password to be? \nMust be longer tha 8 characters or shorter than 128 characters ")
+  );
+  
+  // create conditional statement to check if password length is a num
+  if(Number.isNaN(passwordLength)) {
+    alert("password must be a number")
+    return null;
+  };
+  
+    // cond state to check if pswd is atleast 8 char
+  if (passwordLength < 8) {
+    alert("password length must be atleast 8 characters")
+    return null;
+  };
+  
+    // conditional statement to check if pswd length is atleas 128 characters
+  if (passwordLength > 128) {
+    alert("password can not contain more than 128 characters")
+    return null;
+  };
+  
+  console.log(passwordLength);
+  
+  // create confirmation prompts for types of characters the user wants to use
+    var useChar = window.confirm("Do you want special characters?");
+    var useNumb = window.confirm("Do you want numbers?");
+    var useLowerCase = window.confirm("Do you want lowercase leters?");
+    var useUpperCase = window.confirm("Do you want upper case letters?"); 
+  
+    // create conditional statement to ensure user clicks one of the character types
+    if (useLowerCase == false && useUpperCase == false && useNumeric == false && useSpecial == false) {
+      alert("Please select one type of character");
+      useChar = window.confirm("Do you want special characters?");
+      useNumb = window.confirm("Do you want numbers?");
+      useLowerCase = window.confirm("Do you want lowercase leters?");
+      useUpperCase = window.confirm("Do you want upper case letters?");
+    }
+  
+    // if character type selected, store array into group
+    if (useChar==true){passArray.push(specialChars)};
+    if (useNumb==true){passArray.push(numbArray);}
+    if (useLowerCase==true){passArray.push(lowerCase);}
+    if (useUpperCase==true){passArray.push(upperCase)}
+    
+    // loop to run the length of password
+    for(let i=0;i<passwordLength;i++){
+      
+      // generate random number for group array
+      var randomCharArrayNum;
+      randomCharArrayNum= parseInt(Math.floor(Math.random()*passArray.length)); 
+      
+      //variable that stores selected array from case array
+      var selectedCharArray;
+      selectedCharArray=passArray[randomCharArrayNum]; 
+  
+       
+        console.log("The array has " + passArray.length + " entries"); //testing length of caseArray
+        console.log(randomCharArrayNum); //testing random number generated based on length of caseArray
+      
+      // a generate random number for the character array (length of character array)
+      var randomCharNum;
+      randomCharNum=Math.floor(Math.random()*selectedCharArray.length);
+      
+      console.log("Your random character array num is " + randomCharArrayNum + " and your random character number is " + randomCharNum);
+  
+  
+      // concat string
+      var randomChar;
+      passwordString+=randomChar;
+    }
+  
+    console.log("Your password is " + passwordString);
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 }
 
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", getPassword);
+generateBtn.addEventListener("click", writePassword);
